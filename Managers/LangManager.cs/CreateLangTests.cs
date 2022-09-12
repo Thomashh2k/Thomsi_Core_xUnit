758500d45c4de1dd4cdb36f99e7ba98a76dd61f5
Thomas <t.m3rling@gmail.com>
2022-09-12 20:16:54 +0200

    Commit 120922

---
+++ Managers/LangManager.cs/CreateLangTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using Headless.Core.Payloads;
+using Headless.DB.Tables;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace Headless.Core.xUnit.Managers.LangManager.cs
+{
+    public class CreateLangTests
+    {
+        private Core.Managers.LangManager LangManager { get; set; }
+        private Mock<DB.HeadlessDbContext> MockedDbContext { get; set; }
+        private Mock<DbSet<Lang>> mockLangDbSet { get; set; }
+        public CreateLangTests()
+        {
+            mockLangDbSet = new Mock<DbSet<Lang>>();
+            MockedDbContext = new Mock<DB.HeadlessDbContext>();
+            MockedDbContext.Setup(l => l.Languages).Returns(mockLangDbSet.Object);
+            LangManager = new Core.Managers.LangManager(MockedDbContext.Object);
+        }
+
+        [Fact]
+        public async void CreatePage_Returns_Lang_Object()
+        {
+            CreateLanuagePL mockPL = new CreateLanuagePL
+            {
+                LanguageIdentifier = "de-DE",
+                LanguageName = "German"
+            };
+
+            Lang result = await LangManager.CreateLang(mockPL);
+
+            Assert.IsType<Lang>(result);
+        }
+    }
+}
---
+++ Managers/LangManager.cs/DeleteLangTests.cs
@@ -0,0 +1,39 @@
+﻿using Headless.DB.Tables;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Headless.Core.xUnit.Managers.LangManager.cs
+{
+    public class DeleteLangTests
+    {
+        private Core.Managers.LangManager LangManager { get; set; }
+        private Mock<DB.HeadlessDbContext> MockedDbContext { get; set; }
+        private Mock<DbSet<Lang>> mockLangDbSet { get; set; }
+        public DeleteLangTests()
+        {
+            mockLangDbSet = new Mock<DbSet<Lang>>();
+            MockedDbContext = new Mock<DB.HeadlessDbContext>();
+            MockedDbContext.Setup(l => l.Languages).Returns(mockLangDbSet.Object);
+            LangManager = new Core.Managers.LangManager(MockedDbContext.Object);
+        }
+
+        [Fact]
+        public async void DeleteLang_Returns_true_if_lang_is_found()
+        {
+            var returnedFromFind = new Lang
+            {
+                Id = Guid.NewGuid(),
+                LanguageIdentifier = "de-DE",
+                LanguageName = "German"
+            };
+            mockLangDbSet.Setup(l => l.Find(It.IsAny<Guid>())).Returns(returnedFromFind);
+            bool result = await LangManager.DeleteLang(returnedFromFind.Id);
+            Assert.True(result);
+        }
+    }
+}
